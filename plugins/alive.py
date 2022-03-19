7d4bb81b15d88eca79f7b8f9ab947662d7d67458
Riswanmd16 <99791174+Riswanmd16@users.noreply.github.com>
2022-03-20 00:19:44 +0800

    Update alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -33,7 +33,7 @@ async def _human_time_duration(seconds):
 @Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/da3e7fe6a87f6bfe9e2d3.png",
+        photo=f"https://telegra.ph/file/ef7d073ed69000b4a51e1.png",
         caption=f"""**━━━━━━━━━━━━━━━━━━━━━━━━
 💥 ʜᴇʟʟᴏ, ɪ ᴀᴍ 𝙎𝙪𝙥𝙚𝙧𝙁𝙖𝙨𝙩 𝙑𝘾 𝙁𝙪𝙘𝙠𝙚𝙧 
 ʙᴏᴛ ғᴏʀ ᴛᴇʟᴇɢʀᴀᴍ ɢʀᴏᴜᴘs ...
